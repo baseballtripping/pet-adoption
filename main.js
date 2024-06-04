3a8afb00fc10d9cfc3e371bfb4bcfc856d37c298
Michael <baseballtripping@gmail.com>
2024-06-03 22:21:46 -0700

    pet card

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
+
 async function start() {
   const weatherPromise = await fetch(
     "https://api.weather.gov/gridpoints/VEF/117,92/forecast"
@@ -17,8 +20,25 @@ async function petsArea() {
   );
   const petsData = await petsPromise.json();
   petsData.forEach((pet) => {
-    console.log(pet.name);
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector("h3").textContent = pet.name;
+    clone.querySelector(".pet-age").textContent = petAge(pet.birthYear);
+    clone.querySelector(".pet-description").textContent = pet.description;
+    clone.querySelector(".pet-card-photo img").src = pet.photo;
+    wrapper.appendChild(clone);
   });
+  document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 
 petsArea();
+
+function petAge(age) {
+  const currentYear = new Date().getFullYear();
+  const ages = currentYear - age;
+
+  if (ages == 1) return "1 year old";
+  if (ages == 0) return "Less than a year old";
+
+  return `${ages} years old`;
+}
